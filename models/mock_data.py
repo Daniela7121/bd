ccdbabf2c3306b0aed4e613b845200848262cc0a
Daniela7121 <kurofan8@gmail.com>
2025-05-13 17:15:02 -0600

    Add files via upload

---
+++ models/mock_data.py
@@ -0,0 +1,30 @@
+usuarios_mock = [
+    {
+        "id": 1,
+        "nombre": "Ana Perez",
+        "email": "admin@clinica.com",
+        "contrasena": "admin123",
+        "rol": "admin"
+    },
+    {
+        "id": 2,
+        "nombre": "Luis Torres",
+        "email": "doctor@clinica.com",
+        "contrasena": "doctor123",
+        "rol": "doctor"
+    },
+    {
+        "id": 3,
+        "nombre": "Maria Gomez",
+        "email": "paciente@clinica.com",
+        "contrasena": "paciente123",
+        "rol": "paciente"
+    },
+    {
+        "id": 4,
+        "nombre": "Carlos Ruiz",
+        "email": "administ@clinica.com",
+        "contrasena": "administrativo123",
+        "rol": "administrativo"
+    }
+]
