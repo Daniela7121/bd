64250459784cd04743c72a47c828dc64953e35ec
Daniela7121 <kurofan8@gmail.com>
2025-05-19 18:34:20 -0600

    Update auth_controller.py

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,27 +1,7 @@
-import mysql.connector
-from mysql.connector import Error
+from models.mock_data import usuarios_mock
 
 def autenticar_usuario(nombre, contrasena):
-    try:
-        conexion = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='12345678',
-            database='hospitales'
-        )
-        if conexion.is_connected():
-            cursor = conexion.cursor(dictionary=True)
-            query = """
-                SELECT u.usuario_id AS id, u.nombre, u.email, r.nombre AS rol, u.estado
-                FROM usuarios u
-                JOIN roles r ON u.rol_id = r.rol_id
-                WHERE u.nombre = %s AND u.contrasena = %s AND u.estado = 'activo'
-            """
-            cursor.execute(query, (nombre, contrasena))
-            usuario = cursor.fetchone()
-            cursor.close()
-            conexion.close()
+    for usuario in usuarios_mock:
+        if usuario['nombre'] == nombre and usuario['contrasena'] == contrasena:
             return usuario
-    except Error as e:
-        print(f"Error al conectar o consultar la BD: {e}")
     return None
